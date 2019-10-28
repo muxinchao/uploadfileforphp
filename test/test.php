b6d5e2a762b4113d8bb6ecc33bc851ada3e4bbf5
牟新超 <muxinchao@mouxinchaodeMBP.lan>
2019-10-28 22:56:46 +0800

    update

--- test/test.php
+++ test/test.php
@@ -1,14 +1,7 @@
 <?php 
 require '../vendor/autoload.php';
 
-// use Muxinchao\UploadOne;
 use Muxinchao\Upload;
-
-// echo 'aaa';
-// $file = $_FILES;
-// $obj = new UploadOne();
-// $res = $obj->uploadOne($file);
-// var_dump($res);
 
 $file = $_FILES;
 $obj = new Upload();
